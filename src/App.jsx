a3c53e52cb67d422da67bad47550185495c188c7
Urwa6 <urwa06haider@gmail.com>
2026-09-16 08:55:33 +0200

    Add welcome page image & API setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import {  useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
- 
+
+  const [dogs, setDogs] = useState([])
+useEffect(() => {
+  fetch('https://api.jsonbin.io/v3/b/6aa7c3abac6210605acad585')
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(data => {
+      console.log(data.record.record)
+      setDogs(data.record.record)
+    })
+    .catch(error => console.error('Error fetching dogs:', error))
+}, [])
+
   return (
     <div>
       <header>
@@ -17,11 +32,16 @@ function App() {
     </nav>
       </header>
 
-      <h2>Welcome to Doggy Daycare</h2>
-    
+      <section className="welcome">
+        <h2>Welcome to Doggy Daycare</h2>
+        <p>A safe and fun place for your furry friends!</p>
 
-    <p>A safe and fun place for your furry friends!</p>
-    
+        {
+        dogs.length > 0 && (
+          <img src={dogs[0].image} alt={dogs[0].name} />
+        )}
+        
+      </section>
     </div>
   )
 }
