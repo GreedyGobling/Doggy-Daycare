9878d3348f1a7d049959866837014096e043afe3
wilabson <wilabson@gmail.com>
2026-09-16 10:35:54 +0200

    Moved API to its own file

--- src/components/DogCatalog.jsx
+++ src/components/DogCatalog.jsx
@@ -1,28 +1,13 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
-const apiURL = "https://api.jsonbin.io/v3/b/6aa7c2faffd5d160530465bf";
-const ACCESS_KEY =
-  "$2a$10$zXa7BhCgKbQSrVOx5p3dQuV6kdmQ5SzMEZV4dY.dQf9zWxmkNKro.";
+import { getAllDogs } from "../api/dogs-api";
 
 function DogCatalog() {
   const [dogs, setDogs] = useState([]);
 
   useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(apiURL, {
-        headers: {
-          "X-Access-Key": ACCESS_KEY,
-        },
-      });
-      console.log("Got response: ", response);
-      const data = await response.json();
-      console.log("Got data", data);
-
-      console.log("Dogs:", data.record.record);
-      setDogs(data.record.record);
-    };
-    getData();
+    getAllDogs()
+          .then(setDogs)
   }, []);
 
   if (dogs.length === 0) {
